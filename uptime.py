8e8dc817bed2733f79f1e22f5bb79ef3220ea729
Joel Silva <53149347+zoreu@users.noreply.github.com>
2022-03-24 18:56:08 -0300

    Update uptime.py

--- uptime.py
+++ uptime.py
@@ -1,8 +1,9 @@
 import requests
+import time
 
 def navegador(url):
     try:
-        res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Mobile Safari/537.36'})
+        res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Mobile Safari/537.36'}, timeout=15)
         codigo = res.status_code
     except:
         codigo = 500
@@ -14,11 +15,18 @@ def navegador(url):
 
 def rodar_rotinas():
     navegador('https://rotina.joelsilva3.repl.co/')
+    time.sleep(3)
     navegador('https://telegram.joelsilva3.repl.co/')
+    time.sleep(3)
     navegador('https://statistic.joelsilva3.repl.co/')
+    time.sleep(3)
     navegador('https://painel.joelsilva3.repl.co/')
+    time.sleep(3)
     navegador('https://painel.antonio-carlo22.repl.co/')
+    time.sleep(3)
     navegador('https://apiresolver.joelsilva3.repl.co/')
     
 
+rodar_rotinas()
+rodar_rotinas()
 rodar_rotinas()
